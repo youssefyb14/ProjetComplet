a07d3a0a6ed7a277700696283c5dab848939011b
youssefyb14 <126348823+youssefyb14@users.noreply.github.com>
2024-12-30 21:36:41 +0100

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'PoductProject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PoductProject');
+    expect(app.title).toEqual('PoductProject');  // Vérifiez bien ici la valeur du titre
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PoductProject');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, PoductProject');  // Assurez-vous que cette valeur correspond
   });
 });
